28678285945c4039c63863b8e21a7fee1d2b19d1
vadimpk <vadyman.pk@gmail.com>
2022-05-13 19:49:11 +0300

    message for all update

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import filters
+from aiogram.utils import exceptions
+import asyncio
+
 
 from db.operations import check_if_user_in_db, add_user_to_db, check_if_user_is_admin, get_all_chat_ids
 
@@ -9,7 +13,7 @@ from keyboards.main_menu_keyboard import main_menu_keyboard
 BOT_TOKEN = "5077592058:AAGPmoUShW56UGfVxNGrJ8c0BNHRPqM98-w"
 
 # Initialize bot and dispatcher
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.MARKDOWN_V2)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
@@ -26,10 +30,10 @@ async def send_welcome(message: types.Message):
     # if not check_if_user_in_db(message.from_user.id):
     #     add_user_to_db(message.from_user.id)
 
-    await message.answer("Hi!", reply_markup=main_menu_keyboard)
+    await message.answer("Привіт фреш.", reply_markup=main_menu_keyboard)
 
 
-@dp.message_handler(commands=['admin'])
+@dp.message_handler(filters.Text(contains="Повідомлення для всіх"))
 async def admin(message: types.Message):
     """
     This handler will be called when user sends `/admin` command
@@ -39,13 +43,40 @@ async def admin(message: types.Message):
 
     # TODO: create handler for message that will be sent to all users
     if check_if_user_is_admin(message.from_user.id):
-        msg = "you are admin"
-        for _id in get_all_chat_ids():
-            await bot.send_message(_id, "Повідомлення для всіх")
-    else:
-        msg = "you are not admin"
+        text = message.text.replace("Повідомлення для всіх", "")
+        try:
+            for user_id in get_all_chat_ids():
+                await bot.send_message(user_id, text)
+                await asyncio.sleep(.05)
+        except:
+            await message.reply("Error occurred")
 
-    await message.reply(msg)
+
+async def send_message(user_id: int, text: str, disable_notification: bool = False) -> bool:
+    """
+    Safe messages sender
+    :param user_id:
+    :param text:
+    :param disable_notification:
+    :return:
+    """
+    try:
+        await bot.send_message(user_id, text, disable_notification=disable_notification)
+    except exceptions.BotBlocked:
+        pass
+    except exceptions.ChatNotFound:
+        pass
+    except exceptions.RetryAfter as e:
+        await asyncio.sleep(e.timeout)
+        return await send_message(user_id, text)  # Recursive call
+    except exceptions.UserDeactivated:
+        pass
+    except exceptions.TelegramAPIError:
+        pass
+    else:
+        pass
+        return True
+    return False
 
 
 if __name__ == '__main__':
