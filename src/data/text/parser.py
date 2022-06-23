b308834190e42606a2d245a74502a3f64b0a5f6e
vadimpk <vadyman.pk@gmail.com>
2022-06-23 13:35:07 +0300

    added text for admissions rules

--- src/data/text/parser.py
+++ src/data/text/parser.py
@@ -1,6 +1,17 @@
 def replace_reserved_characters(dict_of_strings):
-    for string in dict_of_strings:
-        dict_of_strings[string] = dict_of_strings[string].replace(".", "\\.") \
-            .replace(",", "\\,").replace(";", "\\;").replace("(", "\\(").replace(")", "\\)") \
-            .replace("!", "\\!").replace("?", "\\?").replace("-", "\\-").replace("<@", "(").replace("@>", ")")
+    for element in dict_of_strings:
+        if type(dict_of_strings[element]) is dict:
+            for string in dict_of_strings[element]:
+                dict_of_strings[element][string] = replace(dict_of_strings[element][string])
+        else:
+            dict_of_strings[element] = replace(dict_of_strings[element])
+
     return dict_of_strings
+
+
+def replace(string):
+    if string.startswith("https"):
+        return string
+    return string.replace(".", "\\.") \
+                .replace(",", "\\,").replace(";", "\\;").replace("(", "\\(").replace(")", "\\)") \
+                .replace("!", "\\!").replace("?", "\\?").replace("-", "\\-").replace("<@", "(").replace("@>", ")")
