9aa75b603bd69859694e9e7317186367f0cb4b57
Stas <stbukovskiy12@gmail.com>
2022-08-07 13:20:33 +0300

    Add function for sending document

--- src/handlers/support/support_handlers.py
+++ src/handlers/support/support_handlers.py
@@ -1,14 +1,13 @@
+import os
+
 from aiogram import Dispatcher, types
 from aiogram.dispatcher import FSMContext
-from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup
-import os
-from dotenv import load_dotenv
-
+from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, ContentTypes
 from data.text.message_text.text import support_message_text, common_message_text
+from dotenv import load_dotenv
 from handlers.states import States
-from keyboards.main_menu_keyboard import main_menu_keyboard
-
 from keyboards.common_buttons import button_exit
+from keyboards.main_menu_keyboard import main_menu_keyboard
 
 load_dotenv()
 CHAT_ID_FOR_BACHELORS = os.getenv("ADMINS_CHAT_ID_FOR_BACHELORS")
@@ -28,27 +27,52 @@ async def get_study_level_command(callback: CallbackQuery, state: FSMContext):
 async def get_question_command(message: Message, state: FSMContext):
     async with state.proxy() as data:
         chat_id = int(CHAT_ID_FOR_BACHELORS if data["study_level"] == "bachelors" else CHAT_ID_FOR_MASTERS)
-    await message.bot.send_message(chat_id=chat_id,
-                                   text="№" + str(message.chat.id) + ";\n\n" +
-                                        message.text,
-                                   parse_mode="HTML")
+    if message.text is not None:
+        await message.bot.send_message(chat_id=chat_id,
+                                       text="№" + str(message.chat.id) + ";\n\n" +
+                                            message.text,
+                                       parse_mode="HTML")
+    elif message.document is not None:
+        await message.bot.send_document(chat_id=chat_id, document=message.document.file_id,
+                                        caption="№" + str(message.chat.id) + ";\n\n" + (message.caption
+                                        if message.caption is not None else "?"),
+                                        parse_mode="HTML")
     await message.answer(support_message_text["thanks_for_question"])
     await message.answer(common_message_text["choose_menu_item"], reply_markup=main_menu_keyboard)
     await state.finish()
 
 
 async def get_answer_command(message: Message):
-    await message.bot.send_message(
-        chat_id=int(message.reply_to_message.text.split(";\n\n")[0][1:]),
-        text=support_message_text["valid_answer_beginning"] +
-             message.reply_to_message.text.split(";\n\n")[1] +
-             support_message_text["valid_answer_end"] +
-             message.text, parse_mode="HTML"
-    )
+    if message.text is not None:
+        await message.bot.send_message(
+            chat_id=int((message.reply_to_message.text if message.reply_to_message.text is not None
+                         else message.reply_to_message.caption).split(";\n\n")[0][1:]),
+            text=support_message_text["valid_answer_beginning"] +
+                 (message.reply_to_message.text if message.reply_to_message.text is not None
+                  else message.reply_to_message.caption).split(";\n\n")[1] +
+                 support_message_text["valid_answer_end"] +
+                 message.text, parse_mode="HTML"
+        )
+    elif message.document is not None:
+        await message.bot.send_document(
+            chat_id=int((message.reply_to_message.text if message.reply_to_message.text is not None
+                         else message.reply_to_message.caption).split(";\n\n")[0][1:]),
+            document=message.document.file_id,
+            caption=support_message_text["valid_answer_beginning"] +
+                    (message.reply_to_message.text if message.reply_to_message.text is not None
+                     else message.reply_to_message.caption).split(";\n\n")[1]
+                    + support_message_text["valid_answer_end"] +
+                    (message.caption if message.caption is not None else ""), parse_mode="HTML"
+        )
 
 
 def register_handlers(dp: Dispatcher):
     dp.register_callback_query_handler(get_study_level_command, state=States.support_main_menu)
-    dp.register_message_handler(get_question_command, state=States.support_main_menu)
+    dp.register_message_handler(get_question_command, state=States.support_main_menu,
+                                content_types=ContentTypes.DOCUMENT)
+    dp.register_message_handler(get_question_command, state=States.support_main_menu,
+                                content_types=ContentTypes.TEXT)
     dp.register_message_handler(get_answer_command, chat_type=[types.ChatType.SUPERGROUP, types.ChatType.GROUP],
-                                is_reply=True)
+                                is_reply=True, content_types=ContentTypes.DOCUMENT)
+    dp.register_message_handler(get_answer_command, chat_type=[types.ChatType.SUPERGROUP, types.ChatType.GROUP],
+                                is_reply=True, content_types=ContentTypes.TEXT)
